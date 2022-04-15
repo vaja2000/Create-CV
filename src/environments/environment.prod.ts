14039c95878b9ceb661f7166d7846fdd3dc69b18
vaja <74463035+vaja2000@users.noreply.github.com>
2022-04-16 03:17:35 +0400

    c

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseCollect: {
+    user: 'user-info'
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyDIVAKrK4gPB3m9BeVbj8Au24pfOPsnuhk",
+    authDomain: "create-cv-c4031.firebaseapp.com",
+    projectId: "create-cv-c4031",
+    storageBucket: "create-cv-c4031.appspot.com",
+    messagingSenderId: "285438073029",
+    appId: "1:285438073029:web:6163ee4c270238371638ac",
+    measurementId: "G-T7H1D38X30"
+  }
 };
